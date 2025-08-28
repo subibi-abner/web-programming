f16eef1d81160ecfa0a377abf925f57865e5a543
subibi-abner <subibiabner@gmail.com>
2025-08-28 09:54:28 +0800

    table.php

--- Activity-02/table.php
+++ Activity-02/table.php
@@ -14,20 +14,25 @@
         array("name"=>"Apple", "price"=>10.50, "stock"=>12),
         array("name"=>"Orange", "price"=>5.60, "stock"=>7),
         array("name"=>"Melon", "price"=>7.00, "stock"=>5),
+        array("name"=>"Banana", "price"=>3.00, "stock"=>15),
+        array("name"=>"Strawberry", "price"=>4.50, "stock"=>8),
+        array("name"=>"Cherry", "price"=>6.00, "stock"=>2),
     );
     ?>
     <table>
         <tr>
+            <th>#</th>
             <th>Product Name</th>
             <th>Price</th>
             <th>Stock</th>
         </tr>
         <?php
-        foreach($products as $p){
+        foreach($products as $index => $p){
            
             $rowClass = $p["stock"] < 10 ? 'class="low-stock"' : '';
         ?>
         <tr <?= $rowClass ?>>
+            <td><?= $index + 1 ?></td>
             <td><?= $p["name"] ?></td>
             <td><?= $p["price"] ?></td>
             <td><?= $p["stock"] ?></td>
